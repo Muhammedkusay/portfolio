f6c36dd3a9f2b93a274a11897f09a3115a2d21e8
muhammedkusay <mhmadhamado35@gmail.com>
2025-07-23 23:37:47 +0300

    Project image slider will be created

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,11 @@ function Projects() {
         <section id="projects" className="px-4 md:px-0 pt-16 md:pt-18 md:mt-32 min-h-[1200px]">
             <Title text={"Projects"} />
             <div className="w-full md:w-3/4 mx-auto mt-12 md:mt-28">
+                
+                {/* make img slider , create slider component*/}
+                <div className="">
+                    
+                </div>
 
             </div>
         </section>
