1c3e485781515f368ae59770f6ced593aeeb5506
muhammedkusay <mhmadhamado35@gmail.com>
2025-07-31 22:47:50 +0300

    Fixed Timline  issues

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -2,7 +2,6 @@ import TimelineCard from "./TimelineCard"
 import firatImg from "/public/assets/firat.png"
 import arslanImg from "/public/assets/arslantek.png"
 import intelliumImg from "/public/assets/intellium-logo.png"
-import intelliumImg2 from "/public/assets/intellium-logo2.png"
 
 function Timeline() {
 
@@ -10,18 +9,18 @@ function Timeline() {
         <div className="mt-24 md:mt-36 lg:mt-44">
             <div className="h-full lg:w-full border-slate-300 border-l lg:border-l-0 lg:border-t flex flex-col lg:flex-row justify-between gap-10 ml-4 lg:ml-0 pl-8 lg:pl-0 lg:pt-8 text-slate-700">
 
-                <TimelineCard month="Sep" year="2022" company="Firat Uni." img={firatImg}>
+                <TimelineCard month="Sep" year="2022" company="Firat Uni.">
                     <p className="pb-2">Started Bachelor's in Software Engineering</p>
                     <p className="pb-8">Graduation in <b>2026</b></p>
                     <img src={firatImg} alt="" className="w-12 mt-auto"/>
                 </TimelineCard>
-                <TimelineCard month="Jul" year="2024" company="Arslan Tech." img={arslanImg}>
+                <TimelineCard month="Jul" year="2024" company="Arslan Tech.">
                     <p className="pb-2">Software Engineer Intern</p>
                     <p>- Fullstack web development</p>
                     <p className="pb-8">- Real estate web application</p>
                     <img src={arslanImg} alt="" className="w-11 mt-auto"/>
                 </TimelineCard>
-                <TimelineCard month="Jul" year="2025" company="Intellium" img={intelliumImg2}>
+                <TimelineCard month="Jul" year="2025" company="Intellium">
                     <p className="pb-2">Software Engineer Intern</p>
                     <p>- Fullstack web development</p>
                     <p className="pb-8">- Project Management System</p>
--- src/components/TimelineCard.jsx
+++ src/components/TimelineCard.jsx
@@ -2,17 +2,16 @@
 function TimelineCard({month, year, company, img, children}) {
 
     return(
-        <div className="flex-1 relative border border-slate-300 rounded-2xl p-4 overflow-hidden">
+        <div className="flex-1 relative border border-slate-300 rounded-2xl p-4">
             
             <div className="absolute -left-15 top-1/2 -translate-y-1/2 lg:-top-9 lg:left-1/2 lg:-translate-x-1/2 bg-white px-2">
-                <span className="hidden lg:inline pr-1">{month}</span>
+                <span className="">{month}</span>
                 {year}
             </div>
 
             <h3 className="font-bold pb-2 text-sm">{company}</h3>
             {children && <div className="h-[83%] flex flex-col text-sm">{children}</div>}
 
-            <img src={img} alt="" className="w-44 absolute -bottom-1/3 -right-1/5 opacity-10" />
         </div>
     )
 }
